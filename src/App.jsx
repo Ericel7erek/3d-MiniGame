5490f32e302cdd28a5ef5e9b27b1b00e5749d3aa
ericel7erek <ericalaa@ymail.com>
2024-05-12 00:48:58 +0200

    miniGame

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,37 @@
 import { Canvas } from "@react-three/fiber";
 import { Experience } from "./components/Experience";
+import { Physics } from "@react-three/rapier";
+import { Suspense, useMemo } from "react";
+import { KeyboardControls } from "@react-three/drei";
+
+export const Controls ={
+  forward:"forward",
+  backward:"backward",
+  left:"left",
+  right:"right",
+  jump:"jump",
+} 
 
 function App() {
+
+  const map = useMemo(()=>[
+    {name: Controls.forward, keys: ["ArrowUp", "KeyW"]},
+    {name: Controls.backward, keys: ["ArrowDown", "KeyS"]},
+    {name: Controls.left, keys: ["ArrowLeft", "KeyA"]},
+    {name: Controls.right, keys: ["ArrowRight", "KeyD"]},
+    {name: Controls.jump, keys: ["Space"]}
+  ],[])
   return (
-    <Canvas shadows camera={{ position: [3, 3, 3], fov: 30 }}>
+    <KeyboardControls map={map}>
+    <Canvas shadows camera={{ position: [20, 20, 20], fov: 30 }}>
       <color attach="background" args={["#ececec"]} />
+      <Suspense>
+        <Physics debug>
       <Experience />
+      </Physics>
+      </Suspense>
     </Canvas>
+    </KeyboardControls>
   );
 }
 
--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,13 +1,103 @@
-import { OrbitControls } from "@react-three/drei";
+import { Box, OrbitControls, Sphere, useKeyboardControls } from "@react-three/drei";
+import { useFrame } from "@react-three/fiber";
+import { RigidBody, quat } from "@react-three/rapier";
+import { useRef, useState } from "react";
+import * as THREE from "three"
+import { Controls } from "../App";
 
 export const Experience = () => {
+
+  const [hover, sethover] = useState(false)
+  const cube = useRef()
+  const kicker = useRef()
+  const [start, setStart] = useState(false)
+  const isOnFLoor = useRef(true)
+  const jump = ()=> {
+    if(isOnFLoor.current) {
+    cube.current.applyImpulse({x: 0, y:5, z: 0})
+    isOnFLoor.current = false
+  }}
+const jumpPressed = useKeyboardControls((state)=> state[Controls.jump])
+const forwardPressed = useKeyboardControls((state)=> state[Controls.forward])
+const backwardPressed = useKeyboardControls((state)=> state[Controls.backward])
+const leftPressed = useKeyboardControls((state)=> state[Controls.left])
+const rightPressed = useKeyboardControls((state)=> state[Controls.right])
+const movement = () => {
+  if(!isOnFLoor.current){
+    return
+  }
+  if(forwardPressed){
+    cube.current.applyImpulse({x: 0, y:0, z: -0.1})
+  }
+  if(backwardPressed){
+    cube.current.applyImpulse({x: 0, y:0, z: 0.1})
+  }
+  if(rightPressed){
+    cube.current.applyImpulse({x: 0.1, y:0, z: 0})
+  }
+  if(leftPressed){
+    cube.current.applyImpulse({x: -0.1, y:0, z: 0})
+  }
+}
+const speed = useRef(5)
+useFrame((_state, delta)=>{
+  movement()
+  if(jumpPressed){
+    jump()
+  }
+  if(!start){
+    return
+  }
+  const rotation = quat(kicker.current.rotation())
+  const newRotation = new THREE.Quaternion().setFromAxisAngle(
+    new THREE.Vector3(0,1,0), 
+    delta * speed.current
+  )
+    rotation.multiply(newRotation)
+    kicker.current.setNextKinematicRotation(rotation)
+    speed.current += delta
+})
   return (
     <>
       <OrbitControls />
-      <mesh>
-        <boxGeometry />
-        <meshNormalMaterial />
-      </mesh>
+      <ambientLight intensity={0.5} />
+
+      {/* <RigidBody colliders={"ball"}  ref={cube}>
+      <Sphere position={[0,10,0]} onPointerEnter={()=>sethover(true)} onPointerLeave={()=>sethover(false)} onClick={jump}>
+        <meshBasicMaterial color={hover? "Pink": "Gold"} />
+        </Sphere>
+      </RigidBody> */}
+      
+      <RigidBody ref={cube} 
+      onCollisionEnter={({other})=>{ 
+        if(other.rigidBodyObject.name === "Floor"){
+          isOnFLoor.current = true;
+        }
+      }}
+      onCollisionLeave={({other})=>{ 
+        if(other.rigidBodyObject.name === "Floor"){
+          isOnFLoor.current = false;
+        }
+      }}
+      >
+      <Box position={[-2,3,0]} args={[1,1,1]} onPointerEnter={()=>sethover(true)} onPointerLeave={()=>sethover(false)} onClick={()=>setStart(true)}>
+        <meshBasicMaterial color={hover? "Pink": "Gold"} />
+        </Box>
+      </RigidBody>
+
+      <RigidBody type="kinematicPosition" position={[0,0.75,0]} ref={kicker}>
+        <group>
+      <Box position={[2.5,0,0]} args={[5,0.5,0.5]}>
+        <meshBasicMaterial color={"#402F1D"} />
+        </Box>
+        </group>
+      </RigidBody>
+
+      <RigidBody type="fixed"  name="Floor">
+      <Box args={[10,1,10]}>
+        <meshBasicMaterial color={"green"} />
+        </Box>
+      </RigidBody>
     </>
   );
 };
